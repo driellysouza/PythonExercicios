52686d4141397329964e995c50553e7ca07090d7
Drielly Souza <driellyalmeida18@gmail.com>
2023-03-31 15:02:28 -0300

    exercício 052

---
+++ ex052.py
@@ -0,0 +1,17 @@
+'''
+Exercício Python 52: Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+'''
+
+num = int(input('Digite um número inteiro: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print(f'\033[33m{c}', end='')
+        tot += 1
+    else:
+        print(f'\033[31m{c}', end='')
+print(f'\n\033[mO número {num} foi divisível {tot} vezes.')
+if tot == 2:
+    print('e por isso ele É PRIMO!')
+else:
+    print('E por isso ele NÃO É PRIMO!')
