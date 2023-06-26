f2e7475b0a5bb75b809436c444a06ccf79e34ee8
Drielly Souza <driellyalmeida18@gmail.com>
2023-06-26 11:11:32 -0300

    exercícios 097

---
+++ ex097.py
@@ -0,0 +1,8 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+escreva('bora pra cima deles')
+escreva('muito fácil. moleza')
+escreva('ulhu')
